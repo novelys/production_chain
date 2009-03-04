8567f1f4242de54d4db8b81061420e5b07391961
slainer68 <slainer68@gmail.com>
2009-03-04 12:11:09 +0100

    Adds Rails method request.bot? to know if the current request comes from a bot.

---
+++ lib/rails/action_controller/abstract_request.rb
@@ -0,0 +1,6 @@
+class ActionController::AbstractRequest
+  # Test if request seems to come from a bot
+  def bot?
+    user_agent =~ /\b(Baidu|Gigabot|Googlebot|libwww-perl|lwp-trivial|msnbot|SiteUptime|Slurp|WordPress|ZIBB|ZyBorg)\b/i
+  end
+end
