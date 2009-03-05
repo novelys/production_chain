2736ea9507739646d6a9437c401e0fe5bd81a33e
slainer68 <slainer68@gmail.com>
2009-03-05 20:00:24 +0100

    Rails 2.2 compatibility.

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -1,7 +1,8 @@
-require 'unicode'
+require 'unicode' unless defined?(Rails) && Rails.version >= "2.2.2"
 
 class String
   def to_slug
+    return parameterize if defined?(Rails) && Rails.version >= "2.2.2"
     str = Unicode.normalize_KD(self).gsub(/[^\x00-\x7F]/n,'')
     str = str.gsub(/\W+/, '-').gsub(/^-+/,'').gsub(/-+$/,'').downcase
   end
